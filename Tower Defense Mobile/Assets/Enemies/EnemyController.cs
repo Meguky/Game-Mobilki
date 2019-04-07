2e752f7e1b17bcb2ad2bc63dd6409fa22d498493
Michał Macikowski <michal.macikowski.1997@gmail.com>
2019-04-07 21:32:43 +0200

    Basic Template
    Enemy movement and logic
    Enemy spawner
    Paceholder map and assets

---
+++ Tower Defense Mobile/Assets/Enemies/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    [SerializeField]
+    private float movementSpeed;
+
+    Vector3 movementDirection;
+    Transform currentDestination;
+    int currentTargetIndex=0;
+
+    void GetNextTarget() {
+
+        if (currentTargetIndex == PathController.waypoints.Length) {
+            Destroy(gameObject);
+            return;
+        }
+        else {
+            movementDirection = (PathController.waypoints[currentTargetIndex].position - transform.position).normalized * movementSpeed;
+            currentDestination = PathController.waypoints[currentTargetIndex];
+            currentTargetIndex++;
+        }
+        
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        GetNextTarget();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(movementDirection * Time.deltaTime, Space.World);
+        if (Vector3.Distance(transform.position, currentDestination.position)<0.2f) {
+            GetNextTarget();
+        }
+    }
+
+}
---
+++ Tower Defense Mobile/Assets/Map/WaveSpawnerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawnerController : MonoBehaviour {
+
+    [SerializeField]
+    EnemyController spawnedEnemy;
+
+    [SerializeField]
+    int numberOfEnemies;
+
+    [SerializeField]
+    float spawnIntervals;
+
+    IEnumerator SpawningCoroutine() {
+        for (int i=0; i<numberOfEnemies; i++) {
+            yield return new WaitForSeconds(spawnIntervals);
+            Instantiate(spawnedEnemy);
+        }
+        yield return new WaitForSeconds(.1f);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(SpawningCoroutine());
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
